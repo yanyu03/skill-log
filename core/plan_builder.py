da4657f602b46fd7abdac8f1e5a813ef66e69e55
yanyu03 <yanyujiangnan03@gmail.com>
2026-08-25 10:23:45 +0800

    Import v1.1 plan baseline

---
+++ core/plan_builder.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+from .scheduler import Task
+
+VALID_MODES={'full','format_only','audit_only','citation_only','template_migration'}
+
+def build_plan(mode='full', *, has_figures=False, research=False, citation_verify=False, roundtrip=True, semantic_validate=True) -> list[Task]:
+    if mode not in VALID_MODES: raise ValueError(mode)
+    tasks=[]
+    def add(id,req,prod,caps=()): tasks.append(Task(id,list(req),list(prod),list(caps)))
+
+    if mode=='citation_only':
+        add('INGEST',[],['NORMALIZED_CONTENT'],['content_extract'])
+        add('SEMANTIC_PARSE',['NORMALIZED_CONTENT'],['CONTENT_READY'])
+        add('CITATION_LOCAL',['CONTENT_READY'],['CITATION_LOCAL_READY'])
+        if citation_verify: add('CITATION_VERIFY',['CITATION_LOCAL_READY'],['CITATION_READY'],['citation_verify'])
+        return tasks
+
+    add('CONFIG_VERIFY',[],['CONFIG_READY'])
+    add('TPL_RESOLVE',['CONFIG_READY'],['TEMPLATE_READY'])
+
+    if mode=='audit_only':
+        add('STRUCTURE_AUDIT',['TEMPLATE_READY'],['STRUCTURE_GATE_PASS'])
+        add('TEMPLATE_AUDIT',['TEMPLATE_READY'],['TEMPLATE_GATE_PASS'])
+        add('RENDER',['TEMPLATE_READY'],['RENDER_READY'],['render'])
+        add('VISUAL_AUDIT',['RENDER_READY'],['VISUAL_GATE_PASS'],['visual_review'])
+        add('FINAL_GATES',['CONFIG_READY','STRUCTURE_GATE_PASS','TEMPLATE_GATE_PASS','VISUAL_GATE_PASS'],['DELIVERY_READY'])
+        return tasks
+
+    add('INGEST',[],['NORMALIZED_CONTENT'],['content_extract'])
+    add('SEMANTIC_PARSE',['NORMALIZED_CONTENT'],['CONTENT_READY'])
+    add('TEMPLATE_CONTAMINATION',['TEMPLATE_READY','CONTENT_READY'],['TEMPLATE_CONTENT_CLEAN'])
+
+    if mode=='full':
+        add('CITATION_LOCAL',['CONTENT_READY'],['CITATION_LOCAL_READY'])
+        if research: add('RESEARCH',['CONTENT_READY'],['RESEARCH_READY'],['research'])
+        if citation_verify: add('CITATION_VERIFY',['CITATION_LOCAL_READY'],['CITATION_READY'],['citation_verify'])
+    elif citation_verify:
+        add('CITATION_LOCAL',['CONTENT_READY'],['CITATION_LOCAL_READY'])
+        add('CITATION_VERIFY',['CITATION_LOCAL_READY'],['CITATION_READY'],['citation_verify'])
+
+    if has_figures: add('FIGURE_PREP',['CONTENT_READY'],['FIGURE_READY'],['figure_assets'])
+
+    if mode=='full' and semantic_validate:
+        sem_req=['CONTENT_READY','CITATION_LOCAL_READY']
+        if research: sem_req.append('RESEARCH_READY')
+        if citation_verify: sem_req.append('CITATION_READY')
+        add('SEMANTIC_AUDIT',sem_req,['SEMANTIC_GATE_PASS'])
+
+    req=['TEMPLATE_READY','CONTENT_READY','TEMPLATE_CONTENT_CLEAN']
+    if mode=='full': req.append('CITATION_LOCAL_READY')
+    if mode=='full' and semantic_validate: req.append('SEMANTIC_GATE_PASS')
+    if has_figures: req.append('FIGURE_READY')
+    if citation_verify: req.append('CITATION_READY')
+    add('OFFICE_COMPOSE',req,['DOCX_READY'],['office_compose'])
+    add('FIELD_REFRESH',['DOCX_READY'],['FIELDS_READY'],['field_refresh'])
+    add('STRUCTURE_AUDIT',['FIELDS_READY'],['STRUCTURE_GATE_PASS'])
+    add('TEMPLATE_AUDIT',['FIELDS_READY'],['TEMPLATE_GATE_PASS'])
+    add('RENDER',['FIELDS_READY'],['RENDER_READY'],['render'])
+    add('VISUAL_AUDIT',['RENDER_READY'],['VISUAL_GATE_PASS'],['visual_review'])
+    if roundtrip:
+        add('ROUNDTRIP',['FIELDS_READY'],['ROUNDTRIP_READY'],['content_extract'])
+        add('CONTENT_AUDIT',['ROUNDTRIP_READY'],['CONTENT_GATE_PASS'])
+    final=['CONFIG_READY','STRUCTURE_GATE_PASS','TEMPLATE_GATE_PASS','VISUAL_GATE_PASS']
+    if mode=='full' and semantic_validate: final.append('SEMANTIC_GATE_PASS')
+    if roundtrip: final.append('CONTENT_GATE_PASS')
+    add('FINAL_GATES',final,['DELIVERY_READY'])
+    add('DELIVERY',['DELIVERY_READY'],['DELIVERED'])
+    return tasks
